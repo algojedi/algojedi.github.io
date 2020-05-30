83363d9d7b4ca0e3284b9abe7af746836eb71d38
Sukhdev <ssbanwait78@gmail.com>
2020-05-30 13:41:50 -0400

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const sideNavToggle = document.querySelector(".navbar_toggle");
+const navLinks = document.querySelectorAll(".navbar_list_item_link");
+console.log(sideNavToggle);
+sideNavToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav_open");
+  navLinks.forEach((link) => {
+    link.classList.add("active");
+    console.log("active class added to each link");
+  });
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav_open");
+    console.log("link clickd");
+  });
+});
