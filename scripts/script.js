f56b0b264f034677cc8cab39ad543dd520deed8e
banwsukh <ssbanwait78@gmail.com>
2020-06-05 17:09:29 -0400

    added scroll effect

--- scripts/script.js
+++ scripts/script.js
@@ -16,10 +16,38 @@ navLinks.forEach((link) => {
   });
 });
 
-// functionality for the cards in projects section
-
-// const card = document.getElementById("card");
-
+// ----------------- functionality for the cards in projects section
 function cardClicked(element) {
   element.classList.toggle("flipped");
 }
+
+// -----------------  functionality for fading in effect
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+// sliders.forEach(slider => {
+//   appearOnScroll.observe(slider);
+// });
